c4c387716eabe3ed2b68c775624e4c0efd3c4c86
Karthik <sdkarthikk@gmail.com>
2019-11-09 12:43:24 +0900

    Update palindrom.java

--- palindrom.java
+++ palindrom.java
@@ -3,61 +3,64 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
-    
-     public static void main(String[] args) {
-        StringBuilder palindromeBuilder=new StringBuilder("");
-        String[] number = getStdin();
-       
-        for (int itr = 0, length = number.length; itr < length; itr++) {
-            String input = String.format(number[itr]);
-            palindromeBuilder.append(input);
+    public static void main(String[] args) {
+        // Scanner scnaer=new Scanner(new InputStreamReader(System.in));
+        // String input = getStdin();
+        String[] lines = getStdin();
+        StringBuilder stringBuilder=new StringBuilder("");
+        for (int i = 0, l = lines.length; i < l; i++) {
+            String input = String.format(lines[i]);
+            stringBuilder.append(input);
+
+            //System.out.println(output);
         }
+        //System.out.println("Enter the input:");
+
+        // if(input.length()>1000000){
+        //     System.out.println("input string exceeds max length of 1000000");
 
         try {
-            Integer.parseInt(palindromeBuilder.toString());
-            String longestPalindrome=searchPalindrome(palindromeBuilder.toString());
-            System.out.println(longestPalindrome);
+            Integer.parseInt(stringBuilder.toString());
+            String longest=longestPalindrome(stringBuilder.toString());
+            System.out.println(longest);
         }catch(Exception e){
             System.out.println("The entered text is not an number!.");
         }
-    }
 
+    }
     private static String[] getStdin() {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> numbers = new ArrayList<>();
-        numbers.add(scanner.nextLine());
-        return numbers.toArray(new String[numbers.size()]);
-    }
+        ArrayList<String> lines = new ArrayList<>();
 
-    public static String findPalindrome(String numberString, int start, int end) {
-        if (start > end) {
-            return null;
-        }
-        while (start >= 0 && end < numberString.length()
-                && numberString.charAt(start) == numberString.charAt(end)) {
+            lines.add(scanner.nextLine());
+
+        return lines.toArray(new String[lines.size()]);
+    }
+    static public String intermediatePalindrome(String s, int start, int end) {
+        if (start > end) return null;
+        while (start >= 0 && end < s.length()
+                && s.charAt(start) == s.charAt(end)) {
             start--;
             end++;
         }
-        return numberString.substring(start + 1, end);
+        return s.substring(start + 1, end);
     }
-
-    public static String searchPalindrome(String numberString) {
-        if (numberString == null) {
-            return null;
-        }
-        String longestPalindrome = numberString.substring(0, 1);
-        for (int itr = 0; itr < numberString.length() - 1; itr++) {
-            String palindrome = findPalindrome(numberString, itr, itr);
-            if (palindrome.length() > longestPalindrome.length()) {
-                longestPalindrome = palindrome;
+    public static String longestPalindrome(String s) {
+        if (s == null) return null;
+        String longest = s.substring(0, 1);
+        for (int i = 0; i < s.length() - 1; i++) {
+            String palindrome = intermediatePalindrome(s, i, i);
+            if (palindrome.length() > longest.length()) {
+                longest = palindrome;
             }
-            palindrome = findPalindrome(numberString, itr, itr + 1);
-            if (palindrome.length() > longestPalindrome.length()) {
-                longestPalindrome = palindrome;
+            palindrome = intermediatePalindrome(s, i, i + 1);
+            if (palindrome.length() > longest.length()) {
+                longest = palindrome;
             }
         }
-        return longestPalindrome;
+        return longest;
     }
 
-   
+
+
 }
